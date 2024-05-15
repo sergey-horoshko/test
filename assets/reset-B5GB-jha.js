e8ec24afa7c36413dec509ac9536a142dece03ee
Cultizm <Liks-ea@mail.ru>
2024-05-16 02:37:28 +0300

    push

---
+++ assets/reset-B5GB-jha.js
@@ -0,0 +1,1 @@
+import{s}from"./request-BAP1f-9B.js";function o(r){return s({url:"/password/email/",method:"post",data:r})}function t(r){return s({url:"/password/reset",method:"post",data:r})}export{t as c,o as r};
