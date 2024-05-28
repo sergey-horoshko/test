4c4055dce445cf658ca0596c626103c3d114a5e1
Cultizm <Liks-ea@mail.ru>
2024-05-28 21:21:31 +0300

    add dist

---
+++ dist/assets/SettingsView-xPcX_bc4.js
@@ -0,0 +1,1 @@
+import{b as e,c,e as n}from"./index-CSIFBmTv.js";const t={};function r(o,s){return c(),n("main",null,"настройки")}const _=e(t,[["render",r]]);export{_ as default};
---
+++ dist/assets/logo-light-CzNcJ0CA.js
@@ -0,0 +1,1 @@
+const s="/test/images/logo-dark.svg",t="/test/images/logo-light.svg";export{s as _,t as a};
